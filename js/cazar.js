f45a8904ca55ea97a19fcfa61232d7f92877ba4f
rozapa1998 <rodrigo1998.rz@gmail.com>
2022-02-20 22:00:19 -0300

    Bootstrap local

--- js/cazar.js
+++ js/cazar.js
@@ -1,98 +1,142 @@
+//----------------------------------Inicializacion Personajes------------------------------------------------------
+
+//Obtenemos Stats del Jugador por Local (Eventualmente DB)
 let StatsJugador = JSON.parse(localStorage.getItem("Jugador"))
-
 console.log(StatsJugador)
+let ATQJugador = StatsJugador.ATQ
+let DEFJugador = StatsJugador.DEF
+let HPJugador = StatsJugador.HP
+//Porcentaje Barra HP (Jugador)
+let porcentajeHPJ = (HPJugador*100)/StatsJugador.HP 
 
-let Enemigo1 = {nombre: "Araña", HP:30 ,DEF:12, ATQ: 4} 
+//Generamos objeto Enemigo
+let Enemigo1 = {nombre: "Araña", HP:30 ,DEF:12, ATQ: 4}
+let ATQEnemigo = Enemigo1.ATQ
+let DEFEnemigo = Enemigo1.DEF
+let HPEnemigo  = Enemigo1.HP
+//Porcentaje Barra HP (Enemigo)
+let porcentajeHPE = (HPEnemigo*100)/Enemigo1.HP 
 
-let registro = document.getElementById("registroPelea")
+//Variables de turno
+let TurnoJ = true
+let TurnoE = false
 
+//---------------------------------Fraces Random para Eventos------------------------------------------------------
+const fraces = [
+    {id:1, frace:"esquivo el ataque con exito"}, 
+    {id:2, frace:"uso su agilidad para evadir el ataque"}, 
+    {id:3, frace:"no fue presa del zarpaso"},
+    {id:4, frace:"bloqueo completamente el daño"},
+    {id:4, frace:"no sufrio ninguna perdida de vida"},
+    {id:4, frace:"se rie de lo debil del ataque"}
+    ]
+
+//--------------------------------------------------Funciones------------------------------------------------------
+
+//Randomizador
 const Random = () =>{
     let Random = Math.floor((Math.random() * (4 - 2 + 1)) + 2)
     return Random
 }
 
-const Atacar = (ATQJugador, DEFEnemigo, HPEnemigo) =>{
-    if ((ATQJugador + Random()) >= DEFEnemigo) {
-        //Ataque exitoso
-         HPEnemigo =  HPEnemigo - ATQJugador
-    }
+//Funciones de los Botones
+
+//---------+ATQ+----------
+const Atacar = () =>{
+    let Ataque = ATQJugador + Random()
+    console.log(Ataque)
+    return HPEnemigo = HPEnemigo - Ataque
+}
+//---------+DEF+----------
+const Defender = () =>{
+    let Defender = DEFJugador + Random()
+    return DEFJugador = DEFJugador + Defender
+}
+//---------+HUI+----------
+const Huir = () =>{
+    let Huir = DEFJugador + Random()
+    console.log(Huir)
 }
 
-
-const fraces = [
-{id:1, frace:"esquivo el ataque con exito"}, 
-{id:2, frace:"uso su agilidad para evadir el ataque"}, 
-{id:3, frace:"no fue presa del zarpaso"},
-{id:4, frace:"bloqueo completamente el daño"},
-{id:4, frace:"no sufrio ninguna perdida de vida"},
-{id:4, frace:"se rie de lo debil del ataque"}
-]
-
-/*Progress Bar*/
-
-
-
-
+//Funcion de Pelea
 const Pelear = () =>{
-    //Stats Jugador
-    let HPJugador = StatsJugador.HP
-    let ATQJugador = StatsJugador.ATQ
-    let DEFJugador = StatsJugador.DEF
-    //Stats Enemigo
-    let HPEnemigo  = Enemigo1.HP
-    let ATQEnemigo = Enemigo1.ATQ
-    let DEFEnemigo = Enemigo1.DEF
+    //Condiconales de vida
+    let vivoE = true
+    let vivoJ = true
+    if(HPJugador <= 0){vivoJ = false}
+    if(HPEnemigo <= 0){vivoE = false}
     
-    registro.innerHTML=`Explorando...`
-    setTimeout(() => {registro.innerHTML=`Un ${Enemigo1.nombre} se cruza en tu camino`}, 3000);
-
-    
-
-}
-
-
-/*const Pelear = () =>{
-}
-
-           if (HPEnemigo <= 0) {
-               break
-           }
-        }else{
-            //Si falla el ataque
-            setTimeout(() => {
-                ImprimirDOM((Enemigo1.nombre + " "+ fraces[Random()].frace))
-            }, 4000);
-            
-        }
-        //Ataque del Enemigo
-        if ((ATQEnemigo + Random()) >= DEFJugador) {
-            HPJugador = HPJugador - ATQEnemigo
-            //Impresion de lo ocurrido
-            setTimeout(() => {
-                ImprimirDOM((Enemigo1.nombre + " pego " + ATQEnemigo))
-            }, 2000);
-
-            setTimeout(() => {
-                ImprimirDOM(("A " + StatsJugador.nombre + " le queda " + HPJugador + " de vida"))
-            }, 3500);
-            //Comprueba Vida
-            if (HPJugador <= 0) {
-                break
-            }
-        }else{
-            //Si falla el ataque
-            setTimeout(() => {
-                ImprimirDOM((StatsJugador.nombre+ " " + fraces[Random()].frace))
-            }, 4000);
-        }
-    }
-
-    if (HPJugador > 0) {
-        setTimeout(() => {
-            ImprimirDOM(("Gano" + StatsJugador.nombre))
-        }, 5000);
+    //Simulamos async con setTimeout()
+    botonCazeria.innerHTML=`Explorando...`
+    registro.innerHTML=`Una noche increiblemente oscura cae sobre ti...`
+    setTimeout(() => {botonCazeria.innerHTML=`Detener Caceria`}, 3000);
+    setTimeout(() => {registro.innerHTML=`Una ${Enemigo1.nombre} se aparece en tu camino`}, 3000);
+    //Funcionalidad de combate
         
-    }else{
-        setTimeout(() => {
-            ImprimirDOM(("Gano" + Enemigo1.nombre))
-        }, 5000);*/
+    console.log("Turno del Jugador")
+}
+//--------------------------------------------------------DOM------------------------------------------------------
+
+//Generacion del Escenario por DOM
+let escenario = document.getElementById("Escenario")
+escenario.innerHTML=`
+<div class="card-body escenario m-5">
+    <div class="position-relative mb-4">
+
+        <div class="position-absolute top-0 start-0 translate-middle">
+            <img src="media/Personaje/Personaje1.gif" alt="Personaje" height="90px" class="mt-4"></img>
+            <div class="progress mt-3">
+                <div class="progress-bar bg-success" role="progressbar" style="width: ${porcentajeHPJ}%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+        </div>
+        
+        <img src="" alt="" class="position-absolute top-0 start-50 translate-middle"></img>
+        
+        <div class="position-absolute top-0 start-100 translate-middle">
+            <img src="media/Enemigos/Araña.gif" alt="Personaje" height="90px" class="mt-4"></img>
+            <div class="progress mt-3">
+                <div class="progress-bar bg-danger" role="progressbar" style="width: ${porcentajeHPE}%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="card-body bg-dark">
+        <div class="row">
+            <div class="col-4">
+                <p class="card-text color-font" id="registroPelea"></p>
+            </div>
+            <div class="col-2 position-relative"></div>
+            <div class="col-2 position-relative">
+                <button class="btn btn-success" id="Atacar">Atacar</button>
+            </div>
+            <div class="col-2">
+                <button class="btn btn-danger" id="Defender">Defenderse</button>
+            </div>
+            <div class="col-2">
+                <button class="btn btn-warning" id="Huir">Huir</button>
+            </div>
+        </div>
+</div>`
+
+
+//Captamos registro para imprimir los eventos
+let registro = document.getElementById("registroPelea")
+
+//Captamos los botones de ATQ/DEF/HUI
+//-----ATQ-----
+let AtaqueB = document.getElementById("Atacar")
+AtaqueB.addEventListener("click", Atacar)
+//-----DEF-----
+let DefenderB = document.getElementById("Defender")
+DefenderB.addEventListener("click", Defender)
+//-----HUI-----
+let HuirB = document.getElementById("Huir")
+HuirB.addEventListener("click", Huir)
+
+//Boton de Iniciar Caceria
+let botonCazeria = document.getElementById("botonCazeria")
+botonCazeria.addEventListener("click", Pelear)
+
+
+
+
